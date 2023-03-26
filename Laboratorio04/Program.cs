a8066ae9c0ec93a059c479edbdfec4f20e319df1
NotGhoul01 <emilioestefancontreras@gmail.com>
2023-03-25 19:32:22 -0600

    Resolver01

--- Laboratorio04/Program.cs
+++ Laboratorio04/Program.cs
@@ -4,9 +4,9 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Nombre: ");
-            Console.WriteLine("Carnet: ");
-            Console.WriteLine("Seccion: ");
+            Console.WriteLine("Nombre:Emilio Contreras ");
+            Console.WriteLine("Carnet:1246423 ");
+            Console.WriteLine("Seccion: 04");
         }
     }
 }
